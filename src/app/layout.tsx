bcc6646afc2b588d87093c71a173a567c97e07e1
ahmedtuhin1028 <aiparsonal430@gmail.com>
2026-09-12 13:11:44 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="bn">
       <body>{children}</body>
